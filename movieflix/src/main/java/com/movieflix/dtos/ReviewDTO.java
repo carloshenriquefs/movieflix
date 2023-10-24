7b7155e4552645bb5ebee0d54febb9ccc8c519ab
Carlos Henrique <ichfs9108@gmail.com>
2023-10-24 01:23:04 -0300

    Implements DTO

---
+++ movieflix/src/main/java/com/movieflix/dtos/ReviewDTO.java
@@ -0,0 +1,41 @@
+package com.movieflix.dtos;
+
+public class ReviewDTO {
+
+    private Long id;
+    private String text;
+    private Long movieId;
+    private Long userId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
